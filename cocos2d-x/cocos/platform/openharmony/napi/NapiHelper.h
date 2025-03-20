45feb0ffaadd99586e56271ca81f21b7ab9a01c6
kardelzhou <kardelzhou@tencent.com>
2025-03-20 19:24:43 +0800

    fix: 增加一层目录

---
+++ cocos2d-x/cocos/platform/openharmony/napi/NapiHelper.h
@@ -0,0 +1,47 @@
+/****************************************************************************
+ Copyright (c) 2022-2023 Xiamen Yaji Software Co., Ltd.
+
+ http://www.cocos.com
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated engine source code (the "Software"), a limited,
+ worldwide, royalty-free, non-assignable, revocable and non-exclusive license
+ to use Cocos Creator solely to develop games on your target platforms. You shall
+ not use Cocos Creator software for developing other software or tools that's
+ used for developing games. You are not granted to publish, distribute,
+ sublicense, and/or sell copies of Cocos Creator.
+
+ The software or tools in this License Agreement are licensed, not sold.
+ Xiamen Yaji Software Co., Ltd. reserves all rights not expressly granted to you.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+****************************************************************************/
+
+#pragma once
+
+#define NODE_ADDON_API_ENABLE_TYPE_CHECK_ON_AS 1
+#define NAPI_DISABLE_CPP_EXCEPTIONS            1
+#define NODE_ADDON_API_DISABLE_DEPRECATED      1
+#include "napi.h"
+
+namespace cocos2d {
+
+class NapiHelper {
+public:
+    static Napi::Env getWorkerEnv();
+    static Napi::Object init(Napi::Env env, Napi::Object exports);
+    static Napi::Value napiCallFunction(const char* functionName);
+    static Napi::Value napiCallFunction(const char *functionName,float duration);
+    static void postMessageToUIThread(const std::string
+                                                              &type,
+                                                          Napi::Value param);
+    static Napi::Value postSyncMessageToUIThread(const std::string& type, Napi::Value param);
+};
+
+}
